4b888a86ae9655084524e4999fe286c2b813a90e
Anshgupta8274 <162897537+Anshgupta8274@users.noreply.github.com>
2026-03-31 20:31:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -46,7 +46,7 @@ io.on("connection", (socket) => {
         console.log("User disconnected");
     });
 });
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
